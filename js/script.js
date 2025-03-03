58de132f79c01b568935146ca81914177981dc51
Zia <ziair3812@gmail.com>
2025-03-03 15:39:26 +0600

    added functionality of adding btn function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,77 @@
 
 const completedButton1 = document.querySelectorAll('#completedButton')[0]
 const completedButton2 = document.querySelectorAll('#completedButton')[1]
-console.log(completedButton2);
+
 
 const completedButton3 = document.querySelectorAll('#completedButton')[2]
 const completedButton4 = document.querySelectorAll('#completedButton')[3]
 const completedButton5 = document.querySelectorAll('#completedButton')[4]
 const completedButton6 = document.querySelectorAll('#completedButton')[5]
+
+// // <script>
+//         // creating a function that add children into ul parent
+//                     // Not Optimized bcz full tree traversal used====
+//                     function addLanguage(langName){
+//                         // step-1: create element:---
+//                         const li = document.createElement('li');
+//                                 li.innerHTML = `${langName}`;
+//                         //step-2: select element & attach with:---
+//                         const targetElement = document.querySelector('.language');
+//                         //step-3: appand element:----
+//                         targetElement.appendChild(li);
+//                 }
+        
+
+
+
+//                 addLanguage('python');
+//                 addLanguage('C++');
+        
+//                 function addOptimezLanguage(langName){
+//                         //More optimized Aproach as compare to above scenarioes:::
+//                         const li = document.createElement('li');
+//                         li.appendChild(document.createTextNode(langName));
+//                         document.querySelector('.language').appendChild(li)
+//                 }
+//                 addOptimezLanguage('Golang');
+        
+        
+//                 // EDIT : (editting element):
+//                     //1st child
+//                 const secondLang = document.querySelector('li:nth-child(2)');
+//                 // secondLang.innerHTML = 'Rust';
+//                  const newli = document.createElement('li');
+//                  newli.textContent = 'Rust';
+//                  secondLang.replaceWith(newli);
+        
+//                  // EDIT : (editting element):
+//                     // 2nd child
+//                 const firstlang = document.querySelector('li:first-child');
+//                 firstlang.outerHTML = '<li>TypeScript</li>'
+        
+//                 // EDIT--(Remove)---: (removing element):
+                
+//                 // const lastLang = document.querySelector('li:last-child');
+//                 // lastLang.remove();
+                    
+
+
+function addInfo(btninfo){
+    const p = document.createElement('p');
+        p.innerText = btninfo;
+    const taskContainer = document.getElementById('task-info');
+    taskContainer.appendChild(p);
+}
+
+function invissible(invissible){
+    invissible.classList.add('hidden');
+}
+
+
+
+
+
+
 
 
 completedButton1.addEventListener("click", function(e){
@@ -19,6 +84,10 @@ completedButton1.addEventListener("click", function(e){
     let currentDone1 = parseInt(taskDone) + 1;
      document.querySelector('#completedTask').innerText = currentDone1;
      currentDone1 = taskDone;
+     btn = document.getElementById('btn1').innerText;
+    addInfo(btn);
+    invissible(completedButton1);
+
     
 })
 
@@ -33,7 +102,10 @@ completedButton2.addEventListener("click", function(e){
     let currentDone1 = parseInt(taskDone) + 1;
      document.querySelector('#completedTask').innerText = currentDone1;
      currentDone1 = taskDone;
-    
+     btn = document.getElementById('btn2').innerText;
+    addInfo(btn);
+    invissible(completedButton2);
+
 })
 
 
@@ -47,7 +119,10 @@ completedButton3.addEventListener("click", function(e){
     let currentDone1 = parseInt(taskDone) + 1;
      document.querySelector('#completedTask').innerText = currentDone1;
      currentDone1 = taskDone;
-    
+     btn = document.getElementById('btn3').innerText;
+    addInfo(btn);
+    invissible(completedButton3);
+
 })
 
 
@@ -61,7 +136,10 @@ completedButton4.addEventListener("click", function(e){
     let currentDone1 = parseInt(taskDone) + 1;
      document.querySelector('#completedTask').innerText = currentDone1;
      currentDone1 = taskDone;
-    
+     btn = document.getElementById('btn4').innerText;
+    addInfo(btn);
+    invissible(completedButton4);
+
 })
 
 
@@ -75,7 +153,10 @@ completedButton5.addEventListener("click", function(e){
     let currentDone1 = parseInt(taskDone) + 1;
      document.querySelector('#completedTask').innerText = currentDone1;
      currentDone1 = taskDone;
-    
+     btn = document.getElementById('btn5').innerText;
+    addInfo(btn);
+    invissible(completedButton5);
+
 })
 
 
@@ -89,5 +170,8 @@ completedButton6.addEventListener("click", function(e){
     let currentDone1 = parseInt(taskDone) + 1;
      document.querySelector('#completedTask').innerText = currentDone1;
      currentDone1 = taskDone;
-    
+     btn = document.getElementById('btn6').innerText;
+    addInfo(btn);
+    invissible(completedButton6);
+
 })
